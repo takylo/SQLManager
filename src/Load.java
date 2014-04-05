d0245b12186999fac725f970da15bc3ac4947d53
takylofr <sebthuret@yahoo.fr>
2014-04-05 19:25:20 +0200

    Manage SQL 0.1
    -Debut du projet
    -Affichage d'un message si c'est le 1er fois que l'utilisateur lance le
    logiciel
    -Si c'est la 1er fois creation d'un fichier de config contenant :
    DB_HOST,DB_USER,DB_PASS editable depuis le logiciel
    -Detection de si il y a ( ou non ) un serveur mysql ON , si non on
    affiche une alert
    -Debut de l'application & du SQLManager

---
+++ src/Load.java
@@ -0,0 +1,9 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+
+public class Load {
+	
+	static JLabel LOGO_APP = new JLabel( new ImageIcon( "data/img/logo.png"));
+	
+}
---
+++ src/ManageApp.java
@@ -0,0 +1,13 @@
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+
+public class ManageApp {
+	public ManageApp(){
+		JFrame _frame = new JFrame();
+		_frame.setSize(1300,700);   
+		_frame.setVisible(true);
+
+	}
+}
